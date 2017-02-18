819980ccb83cca322601c386c1121ccc0adb9166
Joel Parker Henderson <joel@joelparkerhenderson.com>
2017-02-17 17:41:52 -0800

    Add Elixir

---
+++ elixir/date_time_format/lib/date_time_format.ex
@@ -0,0 +1,18 @@
+defmodule DateTimeFormat do
+  @moduledoc """
+  Documentation for DateTimeFormat.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DateTimeFormat.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/date_time_format/test/date_time_format_test.exs
@@ -0,0 +1,8 @@
+defmodule DateTimeFormatTest do
+  use ExUnit.Case
+  doctest DateTimeFormat
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
